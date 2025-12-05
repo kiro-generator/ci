433b52245d90b3a08debc3e5c8632cf66b15ee63
Douglas Chimento <dchimento@gmail.com>
2025-12-05 23:24:01 +0000

    feat: add zod (#2)
    Co-authored-by: Doug Chimento <doug@corsa.finance>

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CI all the things
+
+This action produces a JSON string config output which is used to configure the CI pipeline.
+
+It uses [yq](https://github.com/mikefarah/yq) to merge the YAML configuration files.
+
+See [rust-default.yml](./.github/ci-configs/rust-default.yml) for the default rust CI pipeline configuration
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,74 @@
 import * as core from '@actions/core';
 import fs from 'fs';
+import { z } from 'zod';
+
+const ConfigSchema = z.object({
+  runner: z.string().optional(),
+  global: z.object({
+    ubuntu_packages: z.string().optional(),
+    toolchains: z.array(z.string()),
+    features: z.array(z.string()),
+    rustlog: z.string(),
+    fireblocks: z.object({
+      enabled: z.boolean(),
+    }),
+  }),
+  jobs: z.object({
+    coverage: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      args: z.string(),
+      run: z.string(),
+      matrix: z.object({
+        os: z.array(z.string()),
+        toolchains: z.array(z.string()),
+        features: z.array(z.string()),
+      }),
+    }),
+    fmt: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      run: z.string(),
+    }),
+    clippy: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      flags: z.string(),
+      matrix: z.object({
+        os: z.array(z.string()),
+        toolchains: z.array(z.string()),
+        features: z.array(z.string()),
+      }),
+    }),
+    semver: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+    }),
+    hack: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      run: z.string(),
+    }),
+    doc: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      run: z.string(),
+    }),
+    dependencies: z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      run: z.string(),
+    }),
+    'cargo-sort': z.object({
+      if: z.boolean(),
+      'continue-on-error': z.boolean(),
+      run: z.string(),
+    }),
+  }),
+});
+
+type Config = z.infer<typeof ConfigSchema>;
+/*
 interface Config {
   runner: string;
   global: {
@@ -64,6 +133,7 @@ interface Config {
     };
   };
 }
+*/
 
 export async function run(): Promise<void> {
   try {
@@ -75,7 +145,7 @@ export async function run(): Promise<void> {
     } else {
       configRaw = fs.readFileSync(configFilePath, 'utf8');
     }
-    const finalConfig: Config = JSON.parse(configRaw);
+    const finalConfig: Config = ConfigSchema.parse(JSON.parse(configRaw));
     finalConfig.runner = core.getInput('runner') || 'ubuntu-latest';
     if (!finalConfig.jobs.clippy.matrix.os) {
       finalConfig.jobs.clippy.matrix.os = [];
@@ -92,6 +162,7 @@ export async function run(): Promise<void> {
     core.setOutput('config', JSON.stringify(finalConfig));
   } catch (error) {
     // Fail the workflow run if an error occurs
+    //console.error(error);
     if (error instanceof Error) core.setFailed(error.message);
   }
 }
