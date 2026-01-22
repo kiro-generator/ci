56cce55c2fce62b523a99a8093f404ec5f551e66
doug <dchimento@gmail.com>
2026-01-22 21:40:33 +0000

    rename

--- .github/rust-ci.ts
+++ .github/rust-ci.ts
@@ -1,4 +1,4 @@
-import { createRustWorkflow } from '@carteramesh/ci';
+import { createRustWorkflow } from '@dougefresh/ci';
 
 export default function () {
   return createRustWorkflow().semver(false).build();
