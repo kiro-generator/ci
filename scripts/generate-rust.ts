61206dd8b65ff13379d9afb8ba7fc6eceb8276d3
doug <dchimento@gmail.com>
2026-01-23 16:36:38 +0000

    release workflow

--- scripts/generate-rust.ts
+++ scripts/generate-rust.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env bun
-import { resolve } from "path"
-import { pathToFileURL } from "url"
+import { resolve } from 'path';
+import { pathToFileURL } from 'url';
 
-const configPath = resolve(process.cwd(), ".github/rust-ci.ts")
-const userConfig = await import(pathToFileURL(configPath).href)
-const config = userConfig.default()
+const configPath = resolve(process.cwd(), '.github/rust-ci.ts');
+const userConfig = await import(pathToFileURL(configPath).href);
+const config = userConfig.default();
 
-console.log(JSON.stringify(config, null, 2))
+console.log(JSON.stringify(config, null, 2));
