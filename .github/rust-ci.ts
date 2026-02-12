f08a20d014b51c17e2ce8bdc012e2795abbd5172
doug <dchimento@gmail.com>
2026-02-12 20:04:07 +0000

    cleanup

--- .github/rust-ci.ts
+++ .github/rust-ci.ts
@@ -4,6 +4,7 @@ export default function () {
   return createRustWorkflow()
     .enableMdBook()
     .extra('test-extra', 'echo hello')
+    .disableCoverage()
     .semver(false)
     .disableSanitizers()
     .build();
