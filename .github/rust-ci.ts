bc1e17b5f1deafdd4bb794162e2d7fb554d4da4f
doug <dchimento@gmail.com>
2026-02-17 13:24:19 +0000

    feat: context

--- .github/rust-ci.ts
+++ .github/rust-ci.ts
@@ -1,6 +1,9 @@
 import { createRustWorkflow } from '@dougefresh/ci';
 
 export default function () {
+  if (process.env.CONTEXT) {
+    console.log(`context: ${process.env.CONTEXT}`);
+  }
   return createRustWorkflow()
     .enableMdBook()
     .extra('test-extra', 'echo hello')
